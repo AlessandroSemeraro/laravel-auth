86e88a0dc923a965992ffcf3b40251e0068e438e
AlessandroSemeraro <alesemeraro1994@gmail.com>
2024-02-13 18:18:48 +0100

    added img url

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -17,25 +17,25 @@ class ProjectSeeder extends Seeder
         $projects = array(
             array(
                 "title" => "Vue-Boolzapp",
-                "img_url" => "https://picsum.photos/200/300",
+                "img_url" => "https://i.imgur.com/J9Nm6w7.jpg",
                 "date" => "2023-05-12",
                 "description" => "Realizzare l'esercizio Vue Boolzapp usando Vue 3 come visto finora a lezione."
             ),
             array(
                 "title" => "vite-comics",
-                "img_url" => "https://picsum.photos/200/300",
+                "img_url" => "https://screenshots.codesandbox.io/m5cb4c/2.png",
                 "date" => "2023-12-15",
                 "description" => "Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout"
             ),
             array(
                 "title" => "Vite Yu-Gi-Oh",
-                "img_url" => "https://picsum.photos/200/300",
+                "img_url" => "https://e1.pxfuel.com/desktop-wallpaper/9/667/desktop-wallpaper-yugioh-all-monster-gx-god-cards-overview-1444x1016-yu-gi-oh-cards.jpg",
                 "date" => "2023-12-18",
                 "description" => "Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti.Al caricamento della pagina, effettuate una chiama ajax all'API."
             ),
             array(
                 "title" => "php-dischi-json",
-                "img_url" => "https://www.example.com/immagine2.jpg",
+                "img_url" => "https://images2.corriereobjects.it/methode_image/2016/03/24/Spettacoli/Foto%20Gallery/DORSI11F4BIS_7665053F1_4309_MGZOOM.jpg",
                 "date" => "2024-01-16",
                 "description" => "creare una web-app che permetta di leggere una lista di dischi presente nel nostro server."
             ),
